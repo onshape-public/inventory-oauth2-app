599612304ef3a650623e27f415619ecc42adfad5
mthakare <mthakare@onshape.com>
2020-09-16 10:57:48 +0530

    Added token expiration checks.

--- models/token.js
+++ models/token.js
@@ -6,7 +6,10 @@ var TokenSchema   = new mongoose.Schema({
   access: { type: String, required: true },
   refresh: { type: String, required: false},
   userId: { type: String, required: true },
-  applicationId: { type: String, required: true }
+  applicationId: { type: String, required: true },
+  expiryTime : {type : Number, required : false},
+  dateCreated : {type : Date, required : true},
+  dateModified : {type : Date, required : true}
 });
 
 // Export the Mongoose model
